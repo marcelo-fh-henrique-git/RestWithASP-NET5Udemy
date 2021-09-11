dae2367d3cfcbb56deed87c464a75c5b47612c64
unknown <marcelo.fh.henrique@gmail.com>
2021-09-10 21:12:10 -0300

    Working with Content Negotiation

---
+++ 11_RestWithASPNETUdemy_ContentNegotiation/RestWithASPNETUdemy/RestWithASPNETUdemy/Data/Converter/Implementations/BookConverter.cs
@@ -0,0 +1,50 @@
+﻿using RestWithASPNETUdemy.Data.Converter.Contract;
+using RestWithASPNETUdemy.Data.VO;
+using RestWithASPNETUdemy.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNETUdemy.Data.Converter.Implementations
+{
+    public class BookConverter : IParser<BookVO, Book>, IParser<Book, BookVO>
+    {
+        public BookVO Parse(Book origin)
+        {
+            if (origin == null) return null;
+            return new BookVO
+            {
+                Id = origin.Id,
+                Author = origin.Author,
+                LaunchDate = origin.LaunchDate,
+                Price = origin.Price,
+                Title = origin.Title
+            };
+        }
+
+        public Book Parse(BookVO origin)
+        {
+            if (origin == null) return null;
+            return new Book
+            {
+                Id = origin.Id,
+                Author = origin.Author,
+                LaunchDate = origin.LaunchDate,
+                Price = origin.Price,
+                Title = origin.Title
+            };
+        }
+
+        public List<BookVO> Parse(List<Book> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+
+        public List<Book> Parse(List<BookVO> origin)
+        {
+            if (origin == null) return null;
+            return origin.Select(item => Parse(item)).ToList();
+        }
+    }
+}
