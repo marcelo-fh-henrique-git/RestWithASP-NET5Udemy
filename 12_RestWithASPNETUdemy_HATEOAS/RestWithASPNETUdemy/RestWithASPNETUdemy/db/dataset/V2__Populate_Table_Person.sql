681274dfe0e1ce0454bde539f7a68fb1e47caa5b
unknown <marcelo.fh.henrique@gmail.com>
2021-09-14 21:33:49 -0300

    Working with HATEOAS

---
+++ 12_RestWithASPNETUdemy_HATEOAS/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,6 @@
+﻿INSERT INTO `person` VALUES 
+(1,'Ayrton','Sena','São Paulo - Brasil','Male'),
+(2,'Leonardo','Da Vinci','Anchiano - Itália','Male'),
+(3,'Mahatma','Ghandi','Porbandar - India','Male'),
+(4,'Mohamed Ali','Gandhi','Kentucky - USA','Male'),
+(5,'Nelson','Mandela','Mvezo - South Africa','Male');
